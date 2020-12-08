7b1389b90a32316969943521a0521a2cdd68df31
Rafa García <rafael.garcia@adidas.com>
2020-12-08 02:00:47 +0100

    Exercise 2 - password validator fixed solution

--- 02-password-validator/main.rb
+++ 02-password-validator/main.rb
@@ -2,8 +2,10 @@
 # frozen_string_literal: true
 
 require './password_validator'
+require './password_validator_fixed.rb'
 require 'open-uri'
 
 input = URI.open('passwords.txt').readlines
 
-puts "Valid passwords: #{PasswordValidator.validate(input).size}"
+puts "Valid passwords exercise 1: #{PasswordValidator.validate(input).size}"
+puts "Valid passwords exercise 2: #{PasswordValidatorFixed.validate(input).size}"
