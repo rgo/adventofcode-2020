2d38c488efeb099bf5f6cdbe1caa8025181df601
Rafa García <rafael.garcia@adidas.com>
2020-12-12 01:14:17 +0100

    Day 6 - exercise 2
    Refactor ex. 1 a little bit and prepare ex. 2

--- 06-custom-customs/custom_declaration.rb
+++ 06-custom-customs/custom_declaration.rb
@@ -2,10 +2,24 @@
 
 class CustomDeclaration
   def initialize(input)
-    @answers = input.delete("\n")
+    @answers = input
   end
 
   def count_questions
-    @answers.chars.uniq.size
+    group_everybody_answers.size
+  end
+
+  def count_all_yes_questions
+    everybody = group_everybody_answers.join
+
+    everybody.chars.inject(0) do |sum, question|
+      sum += @answers.split("\n").all? { |line| line.include? question } ? 1 : 0
+    end
+  end
+
+  private
+
+  def group_everybody_answers
+    @answers.delete("\n").chars.uniq
   end
 end
--- 06-custom-customs/spec/custom_declaration_spec.rb
+++ 06-custom-customs/spec/custom_declaration_spec.rb
@@ -4,15 +4,52 @@ require 'spec_helper'
 require './custom_declaration'
 
 RSpec.describe CustomDeclaration do
-  it 'counts questions for a group' do
-    input = <<~EOD
+  let(:input1) {
+    <<~EOD
       abcx
       abcy
       abcz
     EOD
+  }
+  let(:input2_simple) { "abc\n" }
+  let(:input2_multiline_no) {
+    <<~EOD
+      a
+      b
+      c
+    EOD
+  }
+  let(:input2_multiline_yes) {
+    <<~EOD
+      a
+      a
+      a
+    EOD
+  }
 
-    custom = CustomDeclaration.new(input)
+  context 'identifying questions where anyone answered yes' do
+    it 'counts questions for a group' do
+      custom = CustomDeclaration.new(input1)
 
-    expect(custom.count_questions).to eq 6
+      expect(custom.count_questions).to eq 6
+    end
+  end
+
+  context 'identifying questions where everyone answered yes' do
+    it 'counts questions in a line' do
+      custom = CustomDeclaration.new(input2_simple)
+
+      expect(custom.count_all_yes_questions).to eq 3
+    end
+
+    it 'counts questions not everyone yes in multiple lines' do
+      custom = CustomDeclaration.new(input2_multiline_no)
+      expect(custom.count_all_yes_questions).to eq 0
+    end
+
+    it 'counts questions everyone yes in multiple lines' do
+      custom = CustomDeclaration.new(input2_multiline_yes)
+      expect(custom.count_all_yes_questions).to eq 1
+    end
   end
 end
