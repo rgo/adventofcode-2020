e3aae6bce93171aa55019beebefd4521cd8c1588
Rafa García <rafael.garcia@adidas.com>
2020-12-13 20:32:26 +0100

    Day 8 - Implement nop and acc

---
+++ 08-handheld-halting/handheld.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module Handheld
+  class ParseError < StandardError
+  end
+
+  def self.load(code)
+    registers = { accumulator: 0 }
+
+    lines = code.split("\n")
+
+    lines.each do |line|
+      operation, params = line.split(' ')
+
+      instruction = case operation
+                    when 'nop'
+                      Instruction::Nop.build(params)
+                    when 'acc'
+                      Instruction::Acc.build(params)
+                    else
+                      raise ParseError, "Unknow instruction: #{operation}"
+                    end
+      instruction.execute(registers)
+    end
+    registers[:accumulator]
+  end
+
+  module Instruction
+    class Nop
+      def self.build(value)
+        new(value)
+      end
+
+      def initialize(_value) end
+
+      def execute(_register) end
+    end
+
+    class Acc
+      def self.build(value)
+        new(value)
+      end
+
+      def initialize(value)
+        @value = value.to_i
+      end
+
+      def execute(register)
+        register[:accumulator] += @value
+      end
+    end
+  end
+end
---
+++ 08-handheld-halting/spec/handheld_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require './handheld'
+
+RSpec.describe Handheld do
+  it 'has an accumulator register' do
+    accumulator = Handheld.load('')
+
+    expect(accumulator).to eq 0
+  end
+
+  context 'instructions' do
+    it 'nop does nothing' do
+      accumulator = Handheld.load('nop +0')
+
+      expect(accumulator).to eq 0
+    end
+
+    context 'acc' do
+      it 'increases accumulator' do
+        accumulator = Handheld.load('acc +1')
+
+        expect(accumulator).to eq 1
+      end
+      it 'decreases accumulator' do
+        accumulator = Handheld.load('acc -2')
+
+        expect(accumulator).to eq(-2)
+      end
+    end
+
+    context 'jmp' do
+      it 'next x lines'
+      it 'go back x lines'
+    end
+
+    it 'raises exception for unknown instructions' do
+      expect { Handheld.load('mul +42') }.to raise_error(Handheld::ParseError)
+    end
+  end
+end
