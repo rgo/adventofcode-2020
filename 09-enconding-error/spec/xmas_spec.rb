e4bdaaaef25e446dc3c06ac59912697f71da58ec
Rafa García <rafael.garcia@adidas.com>
2020-12-19 23:53:47 +0100

    Day 9 - first tests

--- 09-enconding-error/spec/xmas_spec.rb
+++ 09-enconding-error/spec/xmas_spec.rb
@@ -4,4 +4,32 @@ require 'spec_helper'
 require './xmas'
 
 RSpec.describe Xmas do
+  context 'preamble 25 (1..25)' do
+    let(:input) { Array (1..25)}
+
+    it '26 would be valid' do
+      xmas = Xmas.new(input: input << 26, preamble: 25)
+
+      expect(xmas.valid?).to be true
+    end
+
+    it '49 would be valid' do
+      xmas = Xmas.new(input: input << 49, preamble: 25)
+
+      expect(xmas.valid?).to be true
+    end
+
+    it '100 would be invalid' do
+      xmas = Xmas.new(input: input << 100, preamble: 25)
+
+      expect(xmas.valid?).to be false
+    end
+
+    it '50 would be invalid' do
+      xmas = Xmas.new(input: input << 50, preamble: 25)
+
+      expect(xmas.valid?).to be false
+    end
+  end
+
 end
--- 09-enconding-error/xmas.rb
+++ 09-enconding-error/xmas.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
 class Xmas
-  def initialize(input)
-    
+  def initialize(input:, preamble:)
+    @input = input
+    @preamble = preamble
+  end
+
+  def valid?
+    @input.each_cons(@preamble + 1) do |packet|
+      value = packet[-1]
+      preamble = packet[0..-2]
+
+      if preamble.combination(2).none? { |pair| pair.sum == value }
+        puts "Number #{value} is invalid"
+        return false
+      end
+    end
+
+    true
   end
 end
