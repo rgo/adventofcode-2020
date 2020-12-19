5b631d882de1fadfaaedf882dd65709c2595c095
Rafa García <rafael.garcia@adidas.com>
2020-12-20 00:05:43 +0100

    day 9 - Exercise 1

---
+++ 09-enconding-error/main.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require './xmas'
+require 'open-uri'
+
+input = URI.open('input.txt').readlines.map(&:to_i)
+
+xmas = Xmas.new(preamble: 25, input: input)
+
+xmas.valid?
