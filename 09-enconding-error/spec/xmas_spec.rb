19def1a333455cc5ca001992563e996173743284
Rafa García <rafael.garcia@adidas.com>
2020-12-20 00:05:14 +0100

    Add main test

--- 09-enconding-error/spec/xmas_spec.rb
+++ 09-enconding-error/spec/xmas_spec.rb
@@ -5,7 +5,7 @@ require './xmas'
 
 RSpec.describe Xmas do
   context 'preamble 25 (1..25)' do
-    let(:input) { Array (1..25)}
+    let(:input) { Array(1..25) }
 
     it '26 would be valid' do
       xmas = Xmas.new(input: input << 26, preamble: 25)
@@ -32,4 +32,11 @@ RSpec.describe Xmas do
     end
   end
 
+  it '127 is an invalid number' do
+    input = %w[35 20 15 25 47 40 62 55 65 95 102 117 150 182 127 219 299 277 309 576].map(&:to_i)
+
+    xmas = Xmas.new(input: input, preamble: 5)
+
+    expect(xmas.valid?).to be false
+  end
 end
